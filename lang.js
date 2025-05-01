59939da4de7a38862f52c907ccf448cb86bd81fe
ARISALAB <150359240+ARISALAB@users.noreply.github.com>
2025-05-01 09:14:45 +0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -321,7 +321,7 @@ const translations = {
     "btn-fill-form": "Fill Out the Form",
     "btn-go-back": "Go Back",
     "modal-title": "Registration Form",
-    "form-submitt": "Submit",
+    "btn-submit": "Submit",
 "contact-title": "Contact Us",
     "form-name": "Your Name",
     "contact-placeholder-email": "Your Email",
